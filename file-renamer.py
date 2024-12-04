a933ae3c487c5d93d860b44a66c52587301cb2d9
Igor <igor@MacBook-Air-Igor.local>
2024-12-04 18:21:01 +0300

    used pathlib

--- file-renamer.py
+++ file-renamer.py
@@ -1,31 +1,60 @@
 import os
-import datetime
+import pathlib
+
+TITLE_CLI = '''=====================================
+| Утилита для переименования файлов |
+|           ver. 0.1.0              |
+=====================================
+'''
+current_dir = os.getcwd()
+
+def get_list_dir(name_dir: str):
+    'получает список каталогов/файлов'
+    select_dir = pathlib.Path(name_dir)
+    for item in select_dir.iterdir():
+        print(f"{'#' if item.is_dir() else '->'} {item}")
+
+def get_all_list_dir(name_dir: str, mask: str = '*') -> int:
+    'получает список каталогов/файлов с учетом вложенных'
+    select_dir = pathlib.Path(name_dir)
+    index = 0
+    for item in select_dir.rglob(mask):
+        print(f"{'#' if item.is_dir() else '->'} {item}")
+        index += 1
+    return index
 
 def main_menu():
-    print('Утилита для переименования файлов')
+    print(TITLE_CLI)
 
     while True:
-        ask = input('\nВыберите действие: Посмотреть каталог(1), Выйти(2): ')
+        ask = input('Выберите действие:\nВыбрать каталог(нажми "1"), Выйти(нажми "2"): ')
         if ask == '1':
-            ask_way_file = input('Введите путь к каталогу файлов: ')
-            print('\nСписок каталогов/файлов:\n')
-            for dir_file in os.listdir(ask_way_file):  # переписать через рекурсию
-                if os.path.isdir(f'{ask_way_file}/{dir_file}'):
-                    print('#', dir_file)
-                else:
-                    print(' -', dir_file)
-            ask_rename = input('Переименовать? Да(1), Нет(2): ')
-            if ask_rename == '1':
-                print('Работаем! ====>')
-                for dir_file in os.listdir(ask_way_file):
-                    # print(dir_file)
-                    # проверить наличие даты в наименование файла
-                    # отфарматировать дату по шаблону
-                    # переименовать файл
-                    if os.path.exists(f'{ask_way_file}/{dir_file}') and '.tmp' in f'{ask_way_file}/{dir_file}':
-                        print(dir_file)
-                        # os.rename(f'{ask_way_file}/{dir_file}', f'{ask_way_file}/new{dir_file}')
+            ask_path_dir = input('Введите путь к каталогу файлов: ')
+            mask_filter = input('Выберите маску фильтра поиска:\nДД.ММ.ГГГГ(нажми "1"), ДД-ММ-ГГ(нажми "2"): ')
+            if not mask_filter:
+                mask_filter = '*'
+            if mask_filter == '1':
+                mask_filter = '[0-3][0-9].[0-1][0-2].[1-2][0-9][0-9][0-9]*'
+            if mask_filter == '2':
+                mask_filter = '[0-3][0-9]-[0-1][0-2]-[0-9][0-9]*'
+            if get_all_list_dir(ask_path_dir, mask_filter) > 0:
+                ask_rename = input('Переименовать найденные каталоги/файлы?\n Да(нажми "1"), Нет(нажми "2"): ')
+                if ask_rename == '1':
+                    mask_renamed = input('Выберите маску даты для переименования:\nГГГГ.ММ.ДД(нажми "1"), ГГ.ММ.ДД(нажми "2"): ')
+                    if not mask_renamed:
+                        continue
+                    if mask_renamed == '1':
+                        mask_renamed = '[0-3][0-9].[0-1][0-2].[1-2][0-9][0-9][0-9]*'
+                    if mask_renamed == '2':
+                        mask_renamed = '[0-3][0-9].[0-1][0-2].[0-9][0-9]*'
+                    print('Работаем! ====>')
+                    # for dir_file in os.listdir(ask_path_dir):
+                        # print(dir_file)
+                        # os.rename(f'{ask_way_file}/{dir_file}', f'{ask_way_file}/{dir_file}')
+            else:
+                print('Файлы не найдены!\n')
             continue
+
         else:
             print('\nПока!')
             break
