7563521076c1ab3e7878691d39adc99cd1822d60
Igor <igor@MacBook-Air-Igor.local>
2025-01-14 19:38:25 +0300

    добавлена новая ветка для разработки нового функционала

---
+++ change-dir.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+old_path = Path("/home/user/documents")
+new_path = Path("/home/dir-1/dir-2/dir-3")
+
+# Что надо реализовать
+# определить что поменялось в пути к каталогу
+# варианты различий:
+    # /home/user/dir
+    # /home/user/dir-new
+    # =======================
+    # /home/user/dir
+    # /home/user-new/dir
+    # =======================
+    # /home/user/dir
+    # /home-new/user/dir
+    # ==== исключительный случай когда меняются два каталога, пока не будет реализации ====
+    # /home/dir-1/dir-2/dir-3
+    # /home/dir-1/dir-2-new/dir-3-new
+    # ==== удаление каталога ====
+    # /home/user/dir-1/dir-2
+    # /home/user/dir-2
+
+# Сравнить части пути
+if old_path.parts != new_path.parts and len(old_path.parts) == len(new_path.parts):
+    # Найти различия
+    for old, new in reversed(list(zip(old_path.parts, new_path.parts))):
+        if old != new:
+            print(f"Изменилось: {old} -> {new}")
+elif old_path.parts == new_path.parts:
+    print('Пути одинаковы')
+elif len(old_path.parts) > len(new_path.parts):
+    print('Из пути удален один из каталогов')
+else:
+    print('В путь добавлен каталог')
+
+def find_diff_dir(old_dir: Path, new_dir: Path) -> None:
+    """определяет разницу между двумя путями к каталогу"""
+    pass
