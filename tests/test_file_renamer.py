de46ec7da861ee990c83e4f3d1ae875fb656e215
Igor <igor@MacBook-Air-Igor.local>
2025-01-19 10:35:08 +0300

    рефакторинг теста с использование pathlib

--- tests/test_file_renamer.py
+++ tests/test_file_renamer.py
@@ -5,15 +5,16 @@ import platform
 from datetime import datetime, timedelta
 from win32com.client import Dispatch
 from file_renamer import FileRenamer
+from pathlib import Path
 
 class FileRenamerTests(unittest.TestCase):
     def setUp(self):
         """создает тестовые каталоги и файлы"""
-        self.test_dir = 'test_directory'
+        self.test_dir = Path('test_directory')
         self.test_files = []
 
         if not os.path.exists(self.test_dir):
-            os.makedirs(self.test_dir)
+            self.test_dir.mkdir()
 
         def create_shortcut(target_path, shortcut_path):
             """создает ярлык файла
@@ -23,11 +24,12 @@ class FileRenamerTests(unittest.TestCase):
             """
             if platform.system() == "Windows":
                 shell = Dispatch('WScript.Shell')
-                shortcut = shell.CreateShortCut(shortcut_path)
-                shortcut.Targetpath = target_path
+                shortcut = shell.CreateShortCut(str(shortcut_path))
+                shortcut.Targetpath = str(target_path)
                 shortcut.save()
 
-        def create_mock_directories(base_path: str, current_depth: int = 0, max_depth: int = 5):
+        def create_mock_directories(base_path: Path, current_depth: int = 0,
+            max_depth: int = 5):
             """рекурсивно создает структуру mock-каталогов / файлов"""
             if current_depth >= max_depth:
                 return
@@ -35,52 +37,45 @@ class FileRenamerTests(unittest.TestCase):
             date_prefix = current_date.strftime("%d.%m.%y")
 
             # cоздаем каталог для ярлыков на текущем уровне
-            shortcuts_dir = os.path.join(base_path, f"{date_prefix} shortcuts")
-            os.makedirs(shortcuts_dir, exist_ok=True)
+            shortcuts_dir = base_path / f"{date_prefix} shortcuts"
+            shortcuts_dir.mkdir(exist_ok=True)
 
             # cоздаем от 1 до 3 подкаталогов на каждом уровне
             for index_dir in range(random.randint(1, 3)):
                 dir_name = f"{date_prefix} folder-{index_dir}"
-                dir_path = os.path.join(base_path, dir_name)
-                os.makedirs(dir_path, exist_ok=True)
+                dir_path = base_path / dir_name
+                dir_path.mkdir(exist_ok=True)
+                self.test_files.append(dir_name)
 
                 for index_file in range(random.randint(1, 3)):
                     file_name = f"{date_prefix} file-{index_file}.txt"
-                    file_path = os.path.join(dir_path,file_name)
+                    file_path = dir_path / file_name
+                    self.test_files.append(file_name)
 
                     with open(file_path, 'w') as f:
                         f.write(f"Этот {index_file} файл создан {datetime.now()}")
 
                     # cоздаем ярлык для файла
                     shortcut_name = f"{date_prefix} shortcut_to_file_{index_file}.lnk"
-                    shortcut_path = os.path.join(shortcuts_dir, shortcut_name)
+                    shortcut_path = shortcuts_dir / shortcut_name
                     create_shortcut(file_path, shortcut_path)
 
                 create_mock_directories(dir_path, current_depth + 1, max_depth)
 
         create_mock_directories(self.test_dir)
 
-        for dir_path, dir_names, file_names in os.walk(self.test_dir):
-            for cur_dir in dir_names:
-                self.test_files.append(cur_dir)
-            for file in file_names:
-                self.test_files.append(file)
-
-        self.renamer = FileRenamer(self.test_dir)
+        self.renamer = FileRenamer(str(self.test_dir))
 
     def tearDown(self):
         """удаляет тестовый каталог"""
         try:
-            for root, dirs, files in os.walk(self.test_dir, topdown=False):
-                for name in files:
-                    os.remove(os.path.join(root, name))
-                for name in dirs:
-                    os.rmdir(os.path.join(root, name))
-            os.rmdir(self.test_dir)
-        except FileNotFoundError:
-            print("Каталог не существует")
-        except PermissionError:
-            print("Нет прав доступа")
+            if self.test_dir.exists():
+                for item in self.test_dir.glob('**/*'):
+                    if item.is_file():
+                        item.unlink()
+                    elif item.is_dir():
+                        item.rmdir()
+            self.test_dir.rmdir()
         except Exception as e:
             print(f"Произошла ошибка удаления тестовой директории: {e}")
 
@@ -94,10 +89,11 @@ class FileRenamerTests(unittest.TestCase):
     def test_get_files_list(self):
         """тест получения списка файлов"""
         files = self.renamer.get_files_list()
-        print(files)
+        files_str = [str(f.name) for f in files]
+
         self.assertEqual(len(files), len(self.test_files))
-        # for file in self.test_files:  TODO понять, что с WindowsPath
-        #     self.assertIn(file, files)
+        for file in self.test_files:
+            self.assertIn(file, files_str)
 
     def test_rename_files(self):
         """тест переименования файлов"""
